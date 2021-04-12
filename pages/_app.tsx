4a7f63709e18ce7728945dcfcb994e34ba51c739
ArnabXD <arnabxd@pm.me>
2021-04-12 11:41:17 +0530

    fix dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider>
+    <ThemeProvider attribute="class">
         <Component {...pageProps} />
     </ThemeProvider>
   )
