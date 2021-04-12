ede09bf7b660259557604862de177805ec934b36
ArnabXD <arnabxd@pm.me>
2021-04-12 09:24:04 +0530

    DARK MODE TEST

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider>
+        <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
+
+export default MyApp;
