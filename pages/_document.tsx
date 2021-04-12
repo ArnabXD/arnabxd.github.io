8c752f346bff1a1a0054f58f9a9711310febd584
ArnabXD <arnabxd@pm.me>
2021-04-12 18:23:38 +0530

    dark mode

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head />
-                <body className="bg-white text-black dark:bg-black dark:text-white">
+                <body className="bg-gray-100 text-black dark:bg-black dark:text-white">
                     <Main />
                     <NextScript />
                 </body>
