7810a839b5817f9df8e6eaaf2ce5f0d1df956dad
ArnabXD <arnabxd@pm.me>
2021-04-21 15:39:56 +0530

    fix size in lg

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,15 +8,15 @@ const Home: FC = () => {
 	return (
 		<Layout>
 			<div className="xd-content flex flex-col md:flex-row items-center justify-around md:mx-auto">
-			<DevSvg className="h-72 w-72 md:h-96 md:w-96 md:order-last" />
+				<DevSvg className="h-72 w-72 md:h-96 md:w-96 md:order-last" />
 				<div className="text-left mb-24 md:mb-0">
 					<div className="text-md md:text-2xl font-mono pl-1">Hi, I am</div>
 					<div className="text-5xl md:text-6xl xd-title my-2">Arnab Paryali</div>
-					<div className="text-md md:text-2xl font-mono">
+					<div className="text-md md:text-xl font-mono">
 						<Typewriter
 							onInit={() => console.log("Typing Started")}
 							options={{
-								strings: ["A Developer", "An Open-Source Enthusiast", "I like Node.js & Python"],
+								strings: ["A Developer", "An Open-Source Enthusiast", "I like Node.js & Python", "Freelancer"],
 								autoStart: true,
 								loop: true
 							}}
